d757317e0810bc72faada6ea159b3b3d5cc83bf2
Vitor Marcelino <vitor.marcelino@convergenceworks.com.br>
2020-12-06 01:19:04 -0300

    test: Refatora testes funcionais

--- test/functional/tool.spec.js
+++ test/functional/tool.spec.js
@@ -1,33 +1,93 @@
 'use strict'
-const { test, trait } = use('Test/Suite')('Tool')
+const suite = use('Test/Suite')('Tool')
 
-trait('Test/ApiClient')
+const Tool = use("App/Models/Tool")
 
+suite.trait('Test/ApiClient')
 
-test('insert new tool', async ({ assert, client }) => {
-  const notion = {
+const tools = [
+  {
     title: "Notion",
     link: "https://notion.so",
     description: "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ",
+      tags: [
+        "organization",
+        "planning",
+        "collaboration",
+        "writing",
+        "calendar"
+      ]
+  },
+  {
+    title: "json-server",
+    link: "https://github.com/typicode/json-server",
+    description: "Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.",
     tags: [
-      "organization",
-      "planning",
-      "collaboration",
-      "writing",
-      "calendar"
+        "api",
+        "json",
+        "schema",
+        "node",
+        "github",
+        "rest"
+    ]
+  },
+  {
+    title: "fastify",
+    link: "https://www.fastify.io/",
+    description: "Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.",
+    tags: [
+        "web",
+        "framework",
+        "node",
+        "http2",
+        "https",
+        "localhost"
     ]
   }
+]
 
-  let response = await client.post('/tools').send(notion).end()
-  response.assertStatus(201)
+suite.before(async () => {
+  await tools.map(async t => {
+    let tags = t.tags
+    delete t.tags
+    const tool = await Tool.create(t)
+    await tool.addTags(tags)
+  })
+  let sleep = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  await sleep(200)
 })
 
-test('list tools', async ({ assert, client }) => {
+suite.test('list tools', async ({ assert, client }) => {
   let response = await client.get('/tools').end()
+  assert.equal(response.body.length, tools.length)
   response.assertStatus(200)
 })
 
-test('removing tool', async ({ assert, client }) => {
+suite.test('insert new tool', async ({ assert, client }) => {
+  let response = await client.post('/tools').send({
+    title: "Jira",
+    link: "https://www.atlassian.com/software/jira",
+    description: "The #1 software development tool used by agile teams",
+    tags: [
+      'task',
+      'managerm',
+      'project',
+      'agile'
+    ]
+  }).end()
+  response.assertStatus(201)
+})
+
+suite.test('filter tools', async ({ assert, client }) => {
+  
+  let response = await client.get('/tools?tag=organization').end()
+  response.assertStatus(200)
+  assert.equal(response.body.length, 1)
+})
+
+suite.test('removing tool', async ({ assert, client }) => {
   let response = await client.get('/tools').end()
   response.assertStatus(200)
   const idToRemove = response.body[0].id
